52d2356756a0bb0eaf75cb5fb00b0045176c70f4
Gaby <gaby@gmail.com>
2024-06-24 19:34:38 -0400

    se ingresan cambios

---
+++ Explora/src/app/db.service.ts
@@ -0,0 +1,15 @@
+//import { Injectable } from '@angular/core';
+//import { SQLite, SQlieObject } from '@awesome-cordova-plugins/sqlite/ngx';
+//import { ToastController } from '@ionic/angular'
+//import { BehaviorSubject } from 'rxjs';
+
+
+//@Injectable({
+  providedIn: 'root'
+//})
+//export class DbService {
+
+//public db: SQlieObject;
+
+//constructor(private SQLite: SQLite, ) { }
+//}
